9cf5a2701e58836426833fd08b520dccf7b1ef60
Татьяна Сугина <39735343+internetova@users.noreply.github.com>
2021-12-17 16:16:29 +0300

    создание структуры проекта

---
+++ lib/ui/screens/auth_confirmation_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+/// Экран ввода кода
+class AuthConfirmationScreen extends StatefulWidget {
+  const AuthConfirmationScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthConfirmationScreenState createState() => _AuthConfirmationScreenState();
+}
+
+class _AuthConfirmationScreenState extends State<AuthConfirmationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
